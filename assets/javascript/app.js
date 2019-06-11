c79abb117cd5b3eedaecb42df3919a3f761fbc05
Robert1Kowalski <kowals.robert@gmail.com>
2019-06-10 22:36:29 -0600

    added function shells, basic html

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+var quizContainer = document.getElementById("quiz");
+var resultsContainer = document.getElementById("results");
+var submitButton = document.getElementById('submit');
+
+
+//build quiz,show results
+
+function buildQuiz(){}
+
+function showResults(){}
+
+//display quiz right away
+buildQuiz();
+
+//on submit, show results
+submitButton.addEventListener('click', showResults)
